d547450e105e114bed273fcd6aa5281201505aa9
zippyh <zippyh@icloud.com>
2025-10-29 13:08:56 -0400

    Fixed merge conflict

--- SimpleProgram.java
+++ SimpleProgram.java
@@ -2,5 +2,5 @@ import java.util.*;
 public class SimpleProgram {
   public static void main(String[] args) throws Exception {
     Scanner in = new Scanner(System.in);
-System.out.println(4);}
+System.out.println(20);int x=5;}
 }
