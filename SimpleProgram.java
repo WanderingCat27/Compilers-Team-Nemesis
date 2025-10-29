f5c5974896c3a2646a94ecb7ac08bd585ac15f19
JeremyGerm <jeremyrwiening@gmail.com>
2025-10-29 12:21:32 -0400

    Printing.simple fixed

--- SimpleProgram.java
+++ SimpleProgram.java
@@ -2,5 +2,5 @@ import java.util.*;
 public class SimpleProgram {
   public static void main(String[] args) throws Exception {
     Scanner in = new Scanner(System.in);
-System.out.println(4);}
+int x=5;System.out.println(x);x=in.nextInt();System.out.println(x);x=7;System.out.println(x);}
 }
