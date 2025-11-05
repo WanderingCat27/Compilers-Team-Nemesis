c493d74f2d9d16c25755322bea6080b4e7a9828e
JeremyGerm <jeremyrwiening@gmail.com>
2025-11-05 12:26:16 -0500

    fixed conditional signs

--- SimpleProgram.java
+++ SimpleProgram.java
@@ -1,15 +1,20 @@
 import java.util.*;
-
 public class SimpleProgram {
   public static void main(String[] args) throws Exception {
     Scanner in = new Scanner(System.in);
-    int x = 5;
-    if (x == 3) {
-      x = 4;
-    } else if (!(x == 4)) {
-      x = 44;
-    } else {
-      x = 444;
-    }
-  }
+int x=5;
+if(x>3)
+{
+x=4;
+}
+else
+if(!(x<4))
+{
+x=44;
+}
+else
+{
+x=444;
+}
+}
 }
