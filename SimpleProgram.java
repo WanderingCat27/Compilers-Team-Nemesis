299c7655bd3c0e8c071f12161f53b00c05722119
JeremyGerm <jeremyrwiening@gmail.com>
2025-11-12 11:37:36 -0500

    fullExample1 added for demo

--- SimpleProgram.java
+++ SimpleProgram.java
@@ -2,25 +2,19 @@ import java.util.*;
 public class SimpleProgram {
 static Scanner in = new Scanner(System.in);
   public static void main(String[] args) throws Exception {
-int i=1;
-ArrayList<String> list= new ArrayList<String>();
-Collections.addAll(list, new String[]{"a","b","c"});
-System.out.println(list);
-String d="d";
-System.out.println("replace index 1 with 'd'");
-list.set(i-1, d);
-System.out.println(list);
-String a=list.get(0);
-System.out.println("index 1 of list: ");
+int x=0;
+System.out.println("input number x: ");
+x=in.nextInt();
+ArrayList<Integer> y= new ArrayList<Integer>();
+Collections.addAll(y, new Integer[]{1,2,3,4,5,6,7,8,9,10});
+if(x<10)
+{
+System.out.println("x is less than 10");
+System.out.println(y);
+int a=y.get(x-1);
 System.out.println(a);
-System.out.println("adding '1' to list");
-list.add("1");
-System.out.println(list);
-System.out.println("removing index 2");
-list.remove(1);
-System.out.println(list);
-System.out.println("Clearing list");
-list.clear();
-System.out.println(list);
+y.remove(x-1);
+System.out.println(y);
+}
 }
 }
