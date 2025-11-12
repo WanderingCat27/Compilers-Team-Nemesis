71677b850f91b17dc2a26f8bee45b84c398a9fb0
notdev <none>
2025-11-12 11:22:28 -0500

    get from array var

--- SimpleProgram.java
+++ SimpleProgram.java
@@ -5,6 +5,14 @@ static Scanner in = new Scanner(System.in);
 int i=1;
 ArrayList<String> list= new ArrayList<String>();
 Collections.addAll(list, new String[]{"a","b","c"});
+System.out.println(list);
+String d="d";
+System.out.println("replace index 1 with 'd'");
+list.set(i-1, d);
+System.out.println(list);
 String a=list.get(0);
+System.out.println("index 1 of list: ");
+System.out.println(a);
+System.out.println(list);
 }
 }
